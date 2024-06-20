5831f3ba0c1b144024304a41918ce772dc32b7bf
Aarthis98 <aarthini.sivasamy@outlook.com>
2024-06-20 18:10:59 -0400

    adding conflict

--- src/June_18/Employee.java
+++ src/June_18/Employee.java
@@ -1,2 +1,28 @@
-package June_18;public class Employee {
+package June_18;
+
+public class Employee {
+
+    int id;
+    String name;
+    double salary;
+    String designation;
+    Address address;
+
+    public Employee(int id, String name, double salary, String designation, Address address) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.designation = designation;
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", salary=" + salary +
+                ", designation='" + designation + '\'' +
+                '}';
+    }
 }
