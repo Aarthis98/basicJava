56dfbf37c4d6b8f666be04dd1b19ee0ef6d4b7e2
Aarthis98 <aarthini.sivasamy@outlook.com>
2024-07-13 11:12:19 -0400

    adding files

--- src/Regex/Mobile.java
+++ src/Regex/Mobile.java
@@ -1,2 +1,22 @@
-package Regex;public class Mobile {
+package Regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Mobile {
+    public static void main(String[] args) {
+        String input = "+14371231235";
+        String phone = "[\\+]+[1]+[4]+[0-9]{9}";
+
+        Pattern pattern = Pattern.compile(phone);
+        Matcher matcher = pattern.matcher(input);
+
+        if (matcher.matches()) {
+            System.out.println("Valid... ");
+        } else {
+            System.out.println("Invalid..");
+        }
+
+
+    }
 }
--- src/Regex/Sample.java
+++ src/Regex/Sample.java
@@ -1,2 +1,27 @@
-package Regex;public class Sample {
+package Regex;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Sample {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please provide your valid email id ");
+        String input = sc.nextLine();
+
+       // String input = "user@example.com";
+      //  String regex = "^[\\w.%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,}";
+        String email = "[a-zA-Z0-9_\\ -\\.]+@[a-z]+[\\.][com]{3}";
+
+        Pattern pattern = Pattern.compile(email);
+        Matcher matcher = pattern.matcher(input);
+
+        if (matcher.matches()) {
+            System.out.println("Valid email address");
+        } else {
+            System.out.println("Invalid email address");
+        }
+    }
+
 }
